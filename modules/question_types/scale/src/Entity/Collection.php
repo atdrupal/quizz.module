5991460d7e23e5b5bf1f79401f926d2499b8fafd
Andy Truong <thehongtt@gmail.com>
2014-11-28 17:22:10 +0700

    Define 'scale_collection' entity.

---
+++ modules/question_types/scale/src/Entity/Collection.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace Drupal\scale\Entity;
+
+use Entity;
+
+class Collection extends Entity {
+
+  /** @var int */
+  public $id;
+
+  /** @var string */
+  public $name;
+
+  /** @var string */
+  public $label;
+
+  /** @var bool */
+  public $for_all;
+
+}
