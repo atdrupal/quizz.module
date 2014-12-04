1e86e6268bc88cc343f8405a9f5657b06446aab8
Andy Truong <thehongtt@gmail.com>
2014-12-04 16:47:00 +0700

    Rename AnswerController::getInstance() to ::getHandler().

--- src/Entity/AnswerController.php
+++ src/Entity/AnswerController.php
@@ -37,7 +37,7 @@ class AnswerController extends EntityAPIController {
    * @return \Drupal\quiz_question\QuizQuestionResponse
    *  The appropriate QuizQuestionResponce extension instance
    */
-  public function getInstance($result_id, Question $question = NULL, $answer = NULL, $question_qid = NULL, $question_vid = NULL) {
+  public function getHandler($result_id, Question $question = NULL, $answer = NULL, $question_qid = NULL, $question_vid = NULL) {
     $responses = &drupal_static(__METHOD__, array());
 
     if (is_object($question) && isset($responses[$result_id][$question->vid])) {
--- src/Form/QuizReportForm.php
+++ src/Form/QuizReportForm.php
@@ -64,7 +64,7 @@ class QuizReportForm {
    */
   public static function validateElement(&$element, &$form_state) {
     $question = quiz_question_entity_load($element['qid']['#value'], $element['vid']['#value']);
-    if ($quizQuestionResponse = quiz_answer_controller()->getInstance($element['result_id']['#value'], $question)) {
+    if ($quizQuestionResponse = quiz_answer_controller()->getHandler($element['result_id']['#value'], $question)) {
       $quizQuestionResponse->getReportFormValidate($element, $form_state);
     }
   }
