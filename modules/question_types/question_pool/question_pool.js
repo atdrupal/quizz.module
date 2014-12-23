93081889ce1eab17b6e87af57e6d8cb3c4de4a7a
Andy Truong <thehongtt@gmail.com>
2014-12-23 09:39:07 +0700

    Follow up.

--- modules/question_types/question_pool/question_pool.js
+++
@@ -1,9 +0,0 @@
-(function($) {
-  Drupal.behaviors.question_pool = {
-    attach: function(context, settings) {
-      if (typeof settings.question_pool !== "undefined") {
-        $("#quiz-question-answering-form").submit();
-      }
-    }
-  }
-})(jQuery);
