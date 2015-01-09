9b636d058c3dde2a99b0b220497c99587e11aa30
Andy Truong <thehongtt@gmail.com>
2015-01-09 14:31:49 +0700

    Add views argument plugin: default quiz ID.

--- src/Entity/QuizViewsController.php
+++ src/Entity/QuizViewsController.php
@@ -10,21 +10,21 @@ class QuizViewsController extends EntityDefaultViewsController {
     $data = parent::views_data();
 
     $data['quiz_entity']['view_node']['field'] = array(
-      'title'   => t('Link'),
-      'help'    => t('Provide a simple link to the !quiz.', array('!quiz' => QUIZZ_NAME)),
-      'handler' => 'Drupal\quizz\Entity\Views\QuizEntityLink',
+        'title'   => t('Link'),
+        'help'    => t('Provide a simple link to the !quiz.', array('!quiz' => QUIZZ_NAME)),
+        'handler' => 'Drupal\quizz\Entity\Views\QuizEntityLink',
     );
 
     $data['quiz_entity']['edit_node']['field'] = array(
-      'title'   => t('Edit link'),
-      'help'    => t('Provide a simple link to edit the !quiz.', array('!quiz' => QUIZZ_NAME)),
-      'handler' => 'Drupal\quizz\Entity\Views\QuizEntityEditLink',
+        'title'   => t('Edit link'),
+        'help'    => t('Provide a simple link to edit the !quiz.', array('!quiz' => QUIZZ_NAME)),
+        'handler' => 'Drupal\quizz\Entity\Views\QuizEntityEditLink',
     );
 
     $data['quiz_entity']['delete_node']['field'] = array(
-      'title'   => t('Delete link'),
-      'help'    => t('Provide a simple link to delete the !quiz.', array('!quiz' => QUIZZ_NAME)),
-      'handler' => 'Drupal\quizz\Entity\Views\QuizEntityDeleteLink',
+        'title'   => t('Delete link'),
+        'help'    => t('Provide a simple link to delete the !quiz.', array('!quiz' => QUIZZ_NAME)),
+        'handler' => 'Drupal\quizz\Entity\Views\QuizEntityDeleteLink',
     );
 
     return $data;
