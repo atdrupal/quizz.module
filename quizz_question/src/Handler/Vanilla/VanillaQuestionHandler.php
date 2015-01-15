f88283fd28e74cdef7f2140b26f8f977d316cb3d
Andy Truong <thehongtt@gmail.com>
2015-01-15 15:19:19 +0700

    Question type form: Provide "Manual scoring" config option.

--- quizz_question/src/Handler/Vanilla/VanillaQuestionHandler.php
+++ quizz_question/src/Handler/Vanilla/VanillaQuestionHandler.php
@@ -2,8 +2,25 @@
 
 namespace Drupal\quizz_question\Handler\Vanilla;
 
+use Drupal\quizz_question\Entity\QuestionType;
 use Drupal\quizz_question\QuestionHandler;
 
 class VanillaQuestionHandler extends QuestionHandler {
+
+  /**
+   * {@inheritdoc}
+   */
+  public function questionTypeConfigForm(QuestionType $question_type) {
+    $form = array();
+
+    $form['manual_scoring'] = array(
+        '#type'          => 'checkbox',
+        '#title'         => t('Manual scoring'),
+        '#description'   => t("Manuall scoring user's answer."),
+        '#default_value' => $question_type->getConfig('manual_scoring', 0),
+    );
+
+    return $form;
+  }
 
 }
